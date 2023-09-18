d93fdd5b1960a4e2bfa423816777e65fb80dae9b
Daniel Kiss <flowfast47@gmail.com>
2023-09-18 11:50:30 -0700

    add test to add floor to visit from user

--- Elevator.Tests/ModelTests/ElevatorTests.cs
+++ Elevator.Tests/ModelTests/ElevatorTests.cs
@@ -15,8 +15,18 @@ namespace Elevator.Tests
       ElevatorInBuilding newElevator = new ElevatorInBuilding();
       Assert.AreEqual(typeof(ElevatorInBuilding), newElevator.GetType());
     }
-  
-    //move a floor
+
+    //get floor to visit
+    [TestMethod]
+    // naming convention is MethodOrFieldName_Description_ReturnType
+    public void AddFloor_AddFloorToVisit_Void()
+    {
+      ElevatorInBuilding newElevator = new ElevatorInBuilding();
+      int floorToVisit = 2;
+      List<int> expectedAnswer = new List<int> { 2 };
+      newElevator.AddFloor(floorToVisit);
+      CollectionAssert.AreEqual(newElevator.FloorsToVisit, expectedAnswer);
+    }
 
   }
 }
