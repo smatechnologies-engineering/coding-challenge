159773b77dd1141be407cc4564f758c1f33e4525
Daniel Kiss <flowfast47@gmail.com>
2023-09-18 11:32:00 -0700

    add ElevatorEventObject

--- Elevator/Models/ElevatorEvent.cs
+++ Elevator/Models/ElevatorEvent.cs
@@ -1,7 +1,38 @@
+using System;
+
 namespace Elevator.Models
 {
   public class ElevatorEvent
   {
-    
+    public ElevatorEvent(
+      bool DirectionOfElevatorUp,
+      bool DirectionOfEventUp,
+      bool FloorRequestedBool,
+      bool FloorOn,
+      bool FloorPassedBool,
+      bool FloorStopped,
+      bool CompletedEvent
+      )
+    {
+      this.Now = (DateTimeOffset)DateTime.UtcNow;
+      this.DirectionOfElevatorUp = DirectionOfElevatorUp;
+    }
+    //  Timestamp (Date and time including seconds) - use for timing  // using System;
+    public DateTimeOffset Now { get; set; }
+
+    public bool DirectionOfElevatorUp { get; set; }
+    // bool used to determine if elevator will hit this floor when going up if true
+
+    public bool DirectionOfEventUp { get; set; }
+    // bool used when floor is Requested
+    public bool FloorRequestedBool { get; set; }
+    // int is floor elevator has moved to
+    public int FloorOn { get; set; }
+    // FloorPassedBool bool used when floor is passed
+    public bool FloorPassedBool { get; set; }
+    //  bool used when the floor is stoppedon
+    public bool FloorStopped { get; set; }
+    //  bool event is done
+    public bool CompletedEvent { get; set; }
   }
 }
