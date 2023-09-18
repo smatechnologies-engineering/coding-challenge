e6ca0b52874d686debff7231ee86114da2e70326
Daniel Kiss <flowfast47@gmail.com>
2023-09-18 11:50:59 -0700

    add way to store and add floor

--- Elevator/Models/ElevatorInBuilding.cs
+++ Elevator/Models/ElevatorInBuilding.cs
@@ -4,6 +4,11 @@ namespace Elevator.Models
 {
   public class ElevatorInBuilding
   {
-   
+    public List<int> FloorsToVisit = new List<int> { };
+
+    public void AddFloor(int floor)
+    {
+      FloorsToVisit.Add(floor);
+    }
   }
 }
