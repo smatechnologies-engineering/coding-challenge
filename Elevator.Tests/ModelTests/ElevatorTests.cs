49028a89fe2916fe80fda0e1d610dd0c6f3ecc08
Daniel Kiss <flowfast47@gmail.com>
2023-09-18 12:30:37 -0700

    change test to keep track of events in list

--- Elevator.Tests/ModelTests/ElevatorTests.cs
+++ Elevator.Tests/ModelTests/ElevatorTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Elevator.Models;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Elevator.Tests
 {
@@ -19,29 +20,31 @@ namespace Elevator.Tests
     //get floor to visit
     [TestMethod]
     // naming convention is MethodOrFieldName_Description_ReturnType
-    public void AddFloor_AddFloorToVisit_Void()
+    public void AddFloor_AddFloorToVisit_ListOfEvents()
     {
       ElevatorInBuilding newElevator = new ElevatorInBuilding();
       int floorToVisit = 2;
-      List<int> expectedAnswer = new List<int> { 2 };
-      newElevator.AddFloor(floorToVisit);
-      CollectionAssert.AreEqual(newElevator.FloorsToVisit, expectedAnswer);
+      List<ElevatorEvent> expectedAnswer = newElevator.AddEvent(floorToVisit);
+      Console.WriteLine("2929");
+      Console.WriteLine(JsonConvert.SerializeObject(expectedAnswer));
+      Console.WriteLine(JsonConvert.SerializeObject(newElevator.EventsOrderedByFloor));
+      Console.WriteLine("3232");
+      CollectionAssert.AreEqual(expectedAnswer, newElevator.EventsOrderedByFloor);
     }
 
-    [TestMethod]
-    // naming convention is MethodOrFieldName_Description_ReturnType
-    public void AddFloor_AddFloorToVisitMultipleTimes_Void()
-    {
-      ElevatorInBuilding newElevator = new ElevatorInBuilding();
-      int floorToVisit = 2;
-      int floorToVisit2 = 8;
-      int floorToVisit3 = 5;
-      List<int> expectedAnswer = new List<int> { 2, 8, 5 };
-      newElevator.AddFloor(floorToVisit);
-      newElevator.AddFloor(floorToVisit2);
-      newElevator.AddFloor(floorToVisit3);
-      CollectionAssert.AreEqual(newElevator.FloorsToVisit, expectedAnswer);
-    }
+    // [TestMethod]
+    // public void AddFloor_AddFloorToVisitMultipleTimes_Void()
+    // {
+    //   ElevatorInBuilding newElevator = new ElevatorInBuilding();
+    //   int floorToVisit = 2;
+    //   int floorToVisit2 = 8;
+    //   int floorToVisit3 = 5;
+    //   List<int> expectedAnswer = new List<int> { 2, 8, 5 };
+    //   newElevator.AddFloor(floorToVisit);
+    //   newElevator.AddFloor(floorToVisit2);
+    //   newElevator.AddFloor(floorToVisit3);
+    //   CollectionAssert.AreEqual(newElevator.FloorsToVisit, expectedAnswer);
+    // }
 
   }
 }
