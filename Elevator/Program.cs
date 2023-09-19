23047eb3f7f624d3eae986aa8ae4a4de2e40edda
Daniel Kiss <flowfast47@gmail.com>
2023-09-18 19:25:42 -0700

    adjust program to make elevator run in loop

--- Elevator/Program.cs
+++ Elevator/Program.cs
@@ -1,15 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-bool IsOn = true;
-while (IsOn)
-{
-  Console.WriteLine("Hello, World, enter a floor or q to quit");
-  string floor = Console.ReadLine();
-  Console.WriteLine($"you want floor {floor}");
+﻿using Elevator.Models;
+// See https://aka.ms/new-console-template for more information
+ElevatorInBuilding elevator = new ElevatorInBuilding();
 
-  if (floor == "q")
+while (true)
+{
+  Console.WriteLine("Enter a floor request as a number or 'q' to quit, you can keep adding multiple floors just type them and press enter");
+  string input = Console.ReadLine().Trim();
+  if (input.Equals("q"))
   {
-    IsOn = false;
-    Console.WriteLine("turn elevator off");
+    break;
+  }
+  // check use of out
+  else if (int.TryParse(input, out int floor))
+  {
+    Console.WriteLine("16");
+    elevator.RequestFloor(floor);
+    elevator.Run();
+  }
+
+  if (elevator.direction == Direction.Idle)
+  {
+    Console.WriteLine("Elevator is not moving. Starting elevator...");
+    elevator.Run();
   }
 }
+
 
