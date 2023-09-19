fbdcbae74de494f87124a697c5e6d4b5a497d1c0
Daniel Kiss <flowfast47@gmail.com>
2023-09-18 17:02:40 -0700

    add test showing elevator storing floors to visit

--- Elevator.Tests/ModelTests/ElevatorTests.cs
+++ Elevator.Tests/ModelTests/ElevatorTests.cs
@@ -24,40 +24,29 @@ namespace Elevator.Tests
     {
       ElevatorInBuilding newElevator = new ElevatorInBuilding();
       int floorToVisit = 2;
-      List<ElevatorEvent> expectedAnswer = newElevator.AddEvent(floorToVisit);
-      // Console.WriteLine(JsonConvert.SerializeObject(expectedAnswer));
-      CollectionAssert.AreEqual(expectedAnswer, newElevator.EventsOrderedByFloor);
-    }
-
-    [TestMethod]
-    // naming convention is MethodOrFieldName_Description_ReturnType
-    public void AddFloor_AddFloorToVisitMultipleTimesAndSort_ListOfEvents()
-    {
-      ElevatorInBuilding newElevator = new ElevatorInBuilding();
-      int floorToVisit1 = 2;
-      int floorToVisit2 = 8;
-      int floorToVisit3 = 5;
-      newElevator.AddEvent(floorToVisit1);
-      newElevator.AddEvent(floorToVisit2);
-      List<ElevatorEvent> expectedAnswer = newElevator.AddEvent(floorToVisit3);
-      // List<ElevatorEvent> sample = new List<ElevatorEvent> { new ElevatorEvent(1) };
-      Console.WriteLine(JsonConvert.SerializeObject(expectedAnswer));
-      CollectionAssert.AreEqual(expectedAnswer, newElevator.EventsOrderedByFloor);
+      newElevator.RequestFloor(floorToVisit);
+      List<int> expectedAnswer = new List<int> { 2 };
+      Console.WriteLine(JsonConvert.SerializeObject(newElevator.floorRequests));
+      CollectionAssert.AreEqual(expectedAnswer, newElevator.floorRequests);
     }
 
     // [TestMethod]
-    // public void AddFloor_AddFloorToVisitMultipleTimes_Void()
+    // // naming convention is MethodOrFieldName_Description_ReturnType
+    // public void AddFloor_AddFloorToVisitMultipleTimesAndSort_ListOfEvents()
     // {
     //   ElevatorInBuilding newElevator = new ElevatorInBuilding();
-    //   int floorToVisit = 2;
+    //   int floorToVisit1 = 2;
     //   int floorToVisit2 = 8;
     //   int floorToVisit3 = 5;
-    //   List<int> expectedAnswer = new List<int> { 2, 8, 5 };
-    //   newElevator.AddFloor(floorToVisit);
-    //   newElevator.AddFloor(floorToVisit2);
-    //   newElevator.AddFloor(floorToVisit3);
-    //   CollectionAssert.AreEqual(newElevator.FloorsToVisit, expectedAnswer);
+    //   newElevator.AddEvent(floorToVisit1);
+    //   newElevator.AddEvent(floorToVisit2);
+    //   List<ElevatorEvent> expectedAnswer = newElevator.AddEvent(floorToVisit3);
+    //   // List<ElevatorEvent> sample = new List<ElevatorEvent> { new ElevatorEvent(1) };
+    //   Console.WriteLine(JsonConvert.SerializeObject(expectedAnswer));
+    //   CollectionAssert.AreEqual(expectedAnswer, newElevator.EventsOrderedByFloor);
     // }
+
+
 
   }
 }
