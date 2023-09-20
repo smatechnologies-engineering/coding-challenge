704679a87a4d8913516f38b6afa2737d143d4693
Daniel Kiss <flowfast47@gmail.com>
2023-09-20 12:58:32 -0700

    add enum to TypeOfEvent and include in EvevatorEvent

--- Elevator/Models/ElevatorEvent.cs
+++ Elevator/Models/ElevatorEvent.cs
@@ -2,19 +2,26 @@ using System;
 
 namespace Elevator.Models
 {
-  // Timestamp and asynchronous floor request, every time one occurs.
-  // Timestamp and floor, every time elevator passes a floor.
-  // Timestamp and floor, every time elevator stops at a floor.
-  public class ElevatorEvent
+  // 1-Timestamp and asynchronous floor request, every time one occurs.
+  // 2-Timestamp and floor, every time elevator passes a floor.
+  // 3-Timestamp and floor, every time elevator stops at a floor.
 
   // 3 types are floor request, stop on floor, pass a floor-check if stopped if so 4 second wait otherwise 3 second wait, just add the needed seconds to the timestamp
+  public enum TypeOfEvent
+  {
+    FloorRequest,
+    PassFloor,
+    StopFloor
+  }
+  public class ElevatorEvent
   {
     public DateTimeOffset Now { get; set; }
-    public ElevatorEvent(string type)
+    public TypeOfEvent TypeOfEvent { get; set; }
+    public ElevatorEvent(TypeOfEvent type)
     {
-      this.Now = (DateTimeOffset)DateTime.UtcNow;
-      this.FloorRequested = FloorRequested;
+      Now = (DateTimeOffset)DateTime.UtcNow;
+      TypeOfEvent = type;
     }
-    
+
   }
 }
